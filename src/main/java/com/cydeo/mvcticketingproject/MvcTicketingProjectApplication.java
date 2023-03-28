7ddcac40b4db56f45947e37c7e89dfacd5f722a8
kamilgurgener <kamilgurgener@gmail.com>
2023-03-28 09:48:47 -0400

    Initial commit

---
+++ src/main/java/com/cydeo/mvcticketingproject/MvcTicketingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.mvcticketingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcTicketingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcTicketingProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cydeo/mvcticketingproject/MvcTicketingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.mvcticketingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcTicketingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
