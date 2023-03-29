bbb4dfbea4289121a3c91c304d36d3f797512bde
kamilgurgener <kamilgurgener@gmail.com>
2023-03-29 10:21:05 -0400

    Added other components.

--- src/main/java/com/cydeo/mvcticketingproject/controller/CreateController.java
+++ src/main/java/com/cydeo/mvcticketingproject/controller/CreateController.java
@@ -1,4 +1,24 @@
 package com.cydeo.mvcticketingproject.controller;
 
+import com.cydeo.mvcticketingproject.dto.UserDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
 public class CreateController {
+
+    @GetMapping("/create")
+    public String crateUser(Model model){
+
+        model.addAttribute("user", new UserDTO());
+
+
+
+        return "/user/create";
+    }
+
+
 }
---
+++ src/main/java/com/cydeo/mvcticketingproject/entity/Role.java
@@ -0,0 +1,14 @@
+package com.cydeo.mvcticketingproject.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Role {
+
+    private Long id;
+    private String description;
+}
