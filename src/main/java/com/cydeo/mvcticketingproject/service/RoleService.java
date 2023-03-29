688daa281ffb7fb2a92173912cb154a09675a4bb
kamilgurgener <kamilgurgener@gmail.com>
2023-03-29 11:07:14 -0400

    Added other components.

---
+++ src/main/java/com/cydeo/mvcticketingproject/service/RoleService.java
@@ -0,0 +1,13 @@
+package com.cydeo.mvcticketingproject.service;
+
+import com.cydeo.mvcticketingproject.dto.RoleDTO;
+import com.cydeo.mvcticketingproject.dto.UserDTO;
+
+
+import java.util.List;
+
+public interface RoleService extends CrudService <RoleDTO, Long>{
+
+
+
+}
---
+++ src/main/java/com/cydeo/mvcticketingproject/service/UserService.java
@@ -0,0 +1,10 @@
+package com.cydeo.mvcticketingproject.service;
+
+import com.cydeo.mvcticketingproject.dto.UserDTO;
+
+import java.util.List;
+
+public interface UserService extends CrudService  <UserDTO, String>{
+
+
+}
