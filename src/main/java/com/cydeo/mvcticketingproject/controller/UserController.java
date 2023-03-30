ed2023c54558e09dd85191e2635e2d2f56f86c68
kamilgurgener <kamilgurgener@gmail.com>
2023-03-30 08:51:38 -0400

    Updated User and Role Impl Components

--- src/main/java/com/cydeo/mvcticketingproject/controller/UserController.java
+++ src/main/java/com/cydeo/mvcticketingproject/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.cydeo.mvcticketingproject.controller;
 
 import com.cydeo.mvcticketingproject.dto.UserDTO;
+import com.cydeo.mvcticketingproject.service.RoleService;
+import com.cydeo.mvcticketingproject.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,13 +10,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
-public class CreateController {
+public class UserController {
+
+    RoleService roleService;
+    UserService userService;
+
+    public UserController(RoleService roleService, UserService userService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
 
     @GetMapping("/create")
     public String crateUser(Model model){
 
         model.addAttribute("user", new UserDTO());
-
+        model.addAttribute("roles", roleService.findAll());
+        model.addAttribute("users", userService.findAll());
         return "/user/create";
     }
 
