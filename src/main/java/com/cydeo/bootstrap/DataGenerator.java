5d721df0c674143c2f70ffa9e9ee649d3cf642c3
kamilgurgener <kamilgurgener@gmail.com>
2023-04-02 13:25:16 -0400

    Updated Project create html

--- src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,9 +1,11 @@
 package com.cydeo.bootstrap;
 
+import com.cydeo.dto.ProjectDTO;
 import com.cydeo.dto.RoleDTO;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.enums.Gender;
 import com.cydeo.enums.Status;
+import com.cydeo.service.ProjectService;
 import com.cydeo.service.RoleService;
 import com.cydeo.service.UserService;
 import org.springframework.boot.CommandLineRunner;
@@ -14,14 +16,14 @@ import java.time.LocalDate;
 @Component
 public class DataGenerator implements CommandLineRunner {
 
-   RoleService roleService;
-   UserService userService;
+    RoleService roleService;
+    UserService userService;
+    ProjectService projectService;
 
-
-    public DataGenerator(RoleService roleService, UserService userService) {
+    public DataGenerator(RoleService roleService, UserService userService, ProjectService projectService) {
         this.roleService = roleService;
         this.userService = userService;
-
+        this.projectService = projectService;
     }
 
     @Override
@@ -62,17 +64,13 @@ public class DataGenerator implements CommandLineRunner {
         userService.save(user7);
         userService.save(user8);
 
+        ProjectDTO project1 = new ProjectDTO("Spring MVC","PR001",user1, LocalDate.now(),LocalDate.now().plusDays(25),"Creating Controllers", Status.OPEN);
+        ProjectDTO project2 = new ProjectDTO("Spring ORM","PR002",user2, LocalDate.now(),LocalDate.now().plusDays(10),"Creating Database", Status.IN_PROGRESS);
+        ProjectDTO project3 = new ProjectDTO("Spring Container","PR003",user1, LocalDate.now(),LocalDate.now().plusDays(32),"Creating Container", Status.IN_PROGRESS);
 
-
-
-
-
-
-
-
-
-
-
+        projectService.save(project1);
+        projectService.save(project2);
+        projectService.save(project3);
 
 
 
