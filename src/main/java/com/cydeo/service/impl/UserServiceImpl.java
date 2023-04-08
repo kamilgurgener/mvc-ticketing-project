692a5fe2ef9d87ee86eeb5ee7cab90ef57d71b76
kamilgurgener <kamilgurgener@gmail.com>
2023-04-08 15:35:38 -0400

    Added Project delete function

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.cydeo.service.UserService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl extends AbstractMapService<UserDTO,String> implements UserService {
@@ -36,5 +37,10 @@ public class UserServiceImpl extends AbstractMapService<UserDTO,String> implemen
     @Override
     public UserDTO findById(String id) {
         return super.findById(id);
+    }
+
+    @Override
+    public List<UserDTO> findManagers() {
+        return super.findAll().stream().filter( p -> p.getRole().getId() ==  2).collect(Collectors.toList());
     }
 }
